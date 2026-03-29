dfa526a0b14606bcee9a8496288d715c7966630d
sasha <savylovskij@gmail.com>
2026-03-29 23:48:25 +0300

    chore: add prettier and format codebase

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,15 +1,15 @@
-import {Text, View} from "react-native";
-import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
-import {styled} from "react-native-css";
+import { Text, View } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
+import { styled } from 'react-native-css';
 
 const SafeAreaView = styled(RNSafeAreaView);
 
 const Insights = () => {
-    return (
-        <SafeAreaView className="flex-1 bg-background p-5">
-            <Text>Insights</Text>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text>Insights</Text>
+    </SafeAreaView>
+  );
+};
 
-export default Insights
+export default Insights;
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,16 +1,15 @@
-import {Text, View} from "react-native";
-import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
-import {styled} from "react-native-css";
+import { Text, View } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
+import { styled } from 'react-native-css';
 
 const SafeAreaView = styled(RNSafeAreaView);
 
-
 const Settings = () => {
-    return (
-        <SafeAreaView className="flex-1 bg-background p-5">
-            <Text>Settings</Text>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text>Settings</Text>
+    </SafeAreaView>
+  );
+};
 
-export default Settings
+export default Settings;
