c7e96984b8470ee0ca7aff79135d21ab27317814
sasha <savylovskij@gmail.com>
2026-03-29 23:16:19 +0300

    feat: project setup, routing, tab navigation

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,15 @@
 import {Text, View} from "react-native";
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "react-native-css";
+
+const SafeAreaView = styled(RNSafeAreaView);
+
 
 const Settings = () => {
     return (
-        <View>
+        <SafeAreaView className="flex-1 bg-background p-5">
             <Text>Settings</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,10 +1,14 @@
 import {Text, View} from "react-native";
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "react-native-css";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const Subscriptions = () => {
     return (
-        <View>
+        <SafeAreaView className="flex-1 bg-background p-5">
             <Text>Subscriptions</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
